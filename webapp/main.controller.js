46ee1c5c49e870082f59ab59a1b2448109007bc4
kannade <nickcode@mail.ru>
2017-11-20 12:26:57 +0000

    init 3

--- webapp/main.controller.js
+++ webapp/main.controller.js
@@ -18,7 +18,12 @@ sap.ui.define([
         
         onPressBtn: function () {
 					MessageManager.reportSuccess("привет центр sap!", "приветствие");
-				}
-        
+				},
+				
+		 onPressBtn_2: function () {
+		 	sap.ui.require(["sap/hello/lib/MessageManager"], function(MessageManager_2) {
+					MessageManager_2.reportSuccess("привет мир", "заголовок");	
+				});	
+		 }
 	});
 });
